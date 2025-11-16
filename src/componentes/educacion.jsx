eda61ca7f667e9d712c68c6ad9cf05402c1f35f5
Miguel Ángel Rendón Gaviria <miguelrendonrg@gmail.com>
2025-11-16 18:28:53 -0500

    Subo el archivo de educacion actualizado

--- src/componentes/educacion.jsx
+++ src/componentes/educacion.jsx
@@ -1,16 +1,37 @@
 export default function Educacion() {
+  const estudios = [
+    {
+      institucion: "Federico Sierra Arango",
+      nivel: "Primaria",
+      fecha: "2017",
+    },
+    {
+      institucion: "Federico Sierra Arango",
+      nivel: "Secundaria",
+      fecha: "2018 - 2024",
+    },
+    {
+      institucion: "SENA",
+      nivel: "Auxiliar Administrativo",
+      fecha: "2023 - 2024",
+    },
+    {
+      institucion: "SENA",
+      nivel: "Tecnólogo en Análisis y Desarrollo de Sistemas de Información",
+      fecha: "2025 - Presente",
+    },
+  ];
+
   return (
     <section>
       <h2>Educación</h2>
 
       <ul>
-        <li>Federico Sierra Arango - Primaria (2017)</li>
-        <li>Federico Sierra Arango - Secundaria (2018-2024)</li>
-        <li>Sena - Auxiliar Administrativo (2023-2024)</li>
-        <li>
-          Sena - Tecnólogo en Análisis y Desarrollo de Sistemas de Información
-          (2025-presente)
-        </li>
+        {estudios.map((item, index) => (
+          <li key={index}>
+            <strong>{item.institucion}</strong> — {item.nivel} ({item.fecha})
+          </li>
+        ))}
       </ul>
     </section>
   );
