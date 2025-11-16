3d5480f620938aa1ef0fec0882620f2005bbbe43
Miguel Ángel Rendón Gaviria <miguelrendonrg@gmail.com>
2025-11-16 16:59:45 -0500

    Subo cabecera

---
+++ src/componentes/cabeceracv.jsx
@@ -0,0 +1,9 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Miguel Ángel Rendón Gaviria </h1>
+      <h2>Programador</h2>
+      <p>+57 323 655 4348 | miguelrendonrg@gmail.com | Medellín, Colombia</p>
+    </header>
+  );
+}
