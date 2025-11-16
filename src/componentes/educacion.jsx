866b929209977e1b0d1847d011297484d7028a24
Miguel Ángel Rendón Gaviria <miguelrendonrg@gmail.com>
2025-11-16 17:27:28 -0500

    subo educacion

---
+++ src/componentes/educacion.jsx
@@ -0,0 +1,17 @@
+export default function Educacion() {
+  return (
+    <section>
+      <h2>Educación</h2>
+
+      <ul>
+        <li>Federico Sierra Arango - Primaria (2017)</li>
+        <li>Federico Sierra Arango - Secundaria (2018-2024)</li>
+        <li>Sena - Auxiliar Administrativo (2023-2024)</li>
+        <li>
+          Sena - Tecnólogo en Análisis y Desarrollo de Sistemas de Información
+          (2025-presente)
+        </li>
+      </ul>
+    </section>
+  );
+}
