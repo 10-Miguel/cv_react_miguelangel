b92c80b5a886b3847e2cb0afd01aaf126200b388
Miguel Ángel Rendón Gaviria <miguelrendonrg@gmail.com>
2025-11-16 17:10:01 -0500

    Subo perfil

--- src/componentes/perfil.jsx
+++ src/componentes/perfil.jsx
@@ -1,1 +1,24 @@
+export default function Perfil() {
+  return (
+    <section>
+      <h2>Perfil Profesional</h2>
+      <p>
+        Me llamo Miguel Ángel Rendón Gaviria, soy un programador apasionado por
+        el desarrollo de la tecnología. Tengo experiencia en diversas
+        tecnologías y lenguajes de programación, y me especializo en la creación
+        de aplicaciones web eficientes y escalables. Me considero una persona
+        proactiva, con habilidades para el trabajo en equipo y una fuerte
+        orientación a la resolución de problemas.
+      </p>
 
+      <h3>Habilidades</h3>
+      <ul>
+        <li> Conocimiento del idioma ingles (Nivel 3)</li>
+        <li> Trabajo en equipo</li>
+        <li> Comunicación efectiva</li>
+        <li> Asistencia administrativa</li>
+        <li> Conocimiento agrario y ambiental</li>
+      </ul>
+    </section>
+  );
+}
