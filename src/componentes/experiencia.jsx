aaa2e927b231de940fda81b93768039ee6e76ef8
Miguel Ángel Rendón Gaviria <miguelrendonrg@gmail.com>
2025-11-16 17:22:34 -0500

    Subo experiencia

---
+++ src/componentes/experiencia.jsx
@@ -0,0 +1,12 @@
+export default function Experiencia() {
+  return (
+    <section>
+      <h2>Experiencia</h2>
+
+      <div>
+        <h3>Cargo... </h3>
+        <p>Descripción</p>
+      </div>
+    </section>
+  );
+}
