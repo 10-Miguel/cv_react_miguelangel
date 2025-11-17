b982a4e1afcaab4f81242f0294695583edcb0cc1
Miguel Ángel Rendón Gaviria <miguelrendonrg@gmail.com>
2025-11-16 21:22:21 -0500

    Actualizo diseño de app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import CabeceraCV from "./componentes/cabeceracv";
-import Perfil from "./componentes/perfil";
+import { useState } from "react";
+
+import {
+  experienciasData,
+  educacionData,
+  tecnologiasData,
+  habilidadesData,
+} from "./cvData";
+
 import Experiencia from "./componentes/experiencia";
 import Educacion from "./componentes/educacion";
-import Tecnologias from "./componentes/stacktecnologias";
-import StackTecnologias from "./componentes/stacktecnologias";
-export default function App() {
+import Tecnologias from "./componentes/tecnologias";
+import Habilidades from "./componentes/habilidades";
+import ToggleHabilidades from "./componentes/ToggleHabilidades";
+
+function App() {
+  const [experiencias, _setExperiencias] = useState(experienciasData);
+  const [educacion, _setEducacion] = useState(educacionData);
+  const [tecnologias, _setTecnologias] = useState(tecnologiasData);
+  const [habilidades, _setHabilidades] = useState(habilidadesData);
+
   return (
-    <div>
-      <CabeceraCV />
-      <Perfil />
-      <Experiencia />
-      <Educacion />
-      <StackTecnologias />
-    </div>
+    <main>
+      <h1>Mi Hoja de Vida</h1>
+
+      <Experiencia experiencias={experiencias} />
+      <Educacion estudios={educacion} />
+      <Tecnologias tecnologias={tecnologias} />
+      <ToggleHabilidades Habilidades={habilidades} />
+    </main>
   );
 }
+
+export default App;
